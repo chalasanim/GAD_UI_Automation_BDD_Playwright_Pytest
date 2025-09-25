f2b38bd1a7d34af24da4e2c6e51d4b1f53a8a0e5
chalasanim <44677535+chalasanim@users.noreply.github.com>
2025-09-25 14:20:48 +0100

    Update register_user.py
    finished skeleton

--- tests/step_defs/register_user.py
+++ tests/step_defs/register_user.py
@@ -1,23 +1,13 @@
 from pytest-bdd import scenario,given,when,then
 Feature: User Registration
-
-   When I entered user <firstname>
-   	And I entered user <lastname>
-    And I entered user <email>
-    And I entered user <birthdate>
-    And I entered user <password>
-    And I selected user <avtar>
-    And I clicke <register> button    	  
-   Then User created messaged displayed 
-    And the login screen is launnched
   Examples: 
     | firstname | lastname | email 			| birthdate  | avtar   |
     | test      | test     | test@mail.com  | 10/10/1910 | xxxxx   |
 
-@scenario('register_user.feature' ,'As a new user,I should be able to register to log in')
+@scenario('register_user.feature','As a new user,I should be able to register to log in')
 def register_user():
   pass
-@given( 'I opened the GAD launch page')
+@given('I opened the GAD launch page')
 def launch_gad('url'):
   pass
 @given('I hovered on Login icon and clicked Register option')
@@ -27,6 +17,30 @@ def invoke_register():
 def form_invoked():
   pass
 @when('I entered user <firstname>')
-  def enter_firstname(firstname):
+  def enter_text_field(firstname):
     pass
-
+@when('I entered user <lastname>')
+   def enter_text_field(lastname):
+      pass
+@when('I entered user <email>')
+   def enter_text_field(email):
+      pass
+@when('I entered user <birthdate>')
+   def enter_date_field(birthdate):
+      pass
+@when('I entered user <password>')
+   def enter_password(password):
+      pass
+@when('I selected user <avtar>')
+   def select_user_avatar(avtar):
+      pass
+@when(' And I clicked <register> button')
+   def click_buton(button):
+      pass
+   
+@then ('User created <message> displayed')
+   def check_label(text):
+      pass
+@then ('the <login> screen is launnched')
+   def check_form(form):
+      pass
